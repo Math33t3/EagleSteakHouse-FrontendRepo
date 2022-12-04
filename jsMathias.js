7b94b707ec436a57aad4424bc4ce5cfa9d7cec30
Mathias <math33t3@stud.kea.dk>
2022-12-04 22:00:38 +0100

    andet table forsøg

--- jsMathias.js
+++ jsMathias.js
@@ -133,3 +133,20 @@ class JsGet {
 }
 var jsGet = new JsGet()
 
+function tableCreate(row, col){
+    let body = document.body;
+    let tbl  = document.createElement('table');
+    
+
+    for(let i = 0; i < row; i++){
+        let tr = tbl.insertRow();
+        for(let j = 0; j < col; j++){
+                let td = tr.insertCell();
+                td.appendChild(document.createTextNode(`${i},${j}` ));
+                td.style.border = '1px solid black';
+            }     
+    }
+    body.appendChild(tbl);
+}
+
+tableCreate(4,4);
