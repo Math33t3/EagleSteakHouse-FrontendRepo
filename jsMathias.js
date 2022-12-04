59cf1945b5ef0403708e7abf67708ff421e32d5f
Mathias <math33t3@stud.kea.dk>
2022-12-04 20:41:58 +0100

    js indlæsning- meget tæt på

---
+++ jsMathias.js
@@ -0,0 +1,134 @@
+class JsGet {
+
+    constructor(data) {
+        this.data = data
+    }
+
+    async getPizzaList() {
+        let url = "http://localhost:8080/getPizzaList";
+
+        let response = await fetch(url);
+        this.data = await response.json();
+        this.updateMyPizzaUI();
+    }
+
+    updateMyPizzaUI() {
+        const pizzatable = document.getElementById("pizzaDiv2");
+        const pizzacolumn = document.createElement("pizzaTable2");
+        pizzacolumn.innerHTML = this.data[0].id + ' <br> ' + this.data[0].name + ' <br> ' + this.data[0].description
+            + ' <br> ' + this.data[0].regPrice + ' <br> ' + this.data[0].bigPrice;
+        pizzacolumn.classList.add("menupunkter");
+        pizzacolumn.classList.add("table-div");
+        pizzatable.append(pizzacolumn);
+        // lave et loop der udskriver dem i rækkefølge efter id så nr 1 altid er først.
+    }
+
+    async getBurgerList() {
+        let url = "http://localhost:8080/getBurgerList";
+
+        let response = await fetch(url);
+        this.data = await response.json()
+        this.updateMyBurgerUI()
+    }
+
+    updateMyBurgerUI() {
+        const burgertable = document.getElementById("burgerDiv2");
+        const burgercolumn = document.createElement("burgerTable2");
+
+        burgercolumn.innerHTML = this.data[0].id + ' <br> ' + this.data[0].name
+            + ' <br> ' + this.data[0].regPrice + ' <br> ' + this.data[0].price + ' <br> ';
+        burgercolumn.classList.add("menupunkter");
+        burgercolumn.classList.add("table-div");
+        burgertable.append(burgercolumn);
+
+        const burgercolumn2 = document.createElement("burgerTable");
+
+        burgercolumn2.innerHTML = this.data[1].id + ' <br> ' + this.data[1].name
+            + ' <br> ' + this.data[1].description + ' <br> ' + this.data[1].price;
+        burgercolumn2.classList.add("menupunkter");
+        burgercolumn.classList.add("table-div");
+        burgertable.append(burgercolumn2);
+
+        // lave et loop der udskriver dem i rækkefølge efter id så nr 1 altid er først.
+    }
+    tableCreate() {
+
+
+        const body = document.body;
+
+        const tbl = document.createElement('table');
+        tbl.classList.add("menupunkter");
+        tbl.classList.add("table-div");
+
+
+        for (let i = 0; i < 3; i++) {
+            const tr = tbl.insertRow();
+            for (let j = 0; j < 3; j++) {
+                if (i === 2 && j === 1) {
+                    break;
+                } else {
+                    const td = tr.insertCell();
+                    td.appendChild(document.createTextNode(`Cell I${i}/J${j}`));
+                }
+            }
+        }
+        body.appendChild(tbl);
+    }
+
+    async getBurgerList2() {
+        let url = "http://localhost:8080/getBurgerList";
+
+        let response = await fetch(url);
+        this.data = await response.json()
+        this.burgertableCreate()
+    }
+    burgertableCreate() {
+
+
+        const body = document.body;
+
+        const tbl = document.createElement('table');
+        tbl.classList.add("menupunkter");
+        tbl.classList.add("table-div");
+
+        for(let i = 0; i < 4; i++){
+            if(i==0){
+                tbl.appendChild(document.createTextNode('Burgere'));
+            }
+            if(i==1){
+                tbl.appendChild(document.createTextNode(''));
+            }
+            if(i==2){
+                tbl.appendChild(document.createTextNode('Indhold'));
+            }
+            if(i==3){
+                tbl.appendChild(document.createTextNode('Pris'));
+            }
+            
+        }
+        
+        for (let i = 0; i < 3; i++) {
+            const tr = tbl.insertRow();
+
+            for (let j = 0; j < 4; j++) {
+                if (j == 0) {
+                    const td = tr.insertCell();
+                    td.appendChild(document.createTextNode(this.data[i].id));
+                } if (j == 1) {
+                    const td = tr.insertCell();
+                    td.appendChild(document.createTextNode(this.data[i].name));
+                } if (j == 2) {
+                    const td = tr.insertCell();
+                    td.appendChild(document.createTextNode(this.data[i].description));
+                } if (j == 3) {
+                    const td = tr.insertCell();
+                    td.appendChild(document.createTextNode(this.data[i].price));
+                }
+            }
+        }
+
+        body.appendChild(tbl);
+    }
+}
+var jsGet = new JsGet()
+
