687be68dc694a338f7e3b827fb61544bd840e6b4
Mathias <math33t3@stud.kea.dk>
2022-12-06 12:25:08 +0100

    tilføjelse til menu JS

--- jsMathias.js
+++ jsMathias.js
@@ -363,24 +363,3 @@ class JsGet {
 }
 
 var jsGet = new JsGet()
-
-
-
-//dette er et forsøg på en anden slags table
-function tableCreate(row, col){
-    let body = document.body;
-    let tbl  = document.createElement('table');
-    
-
-    for(let i = 0; i < row; i++){
-        let tr = tbl.insertRow();
-        for(let j = 0; j < col; j++){
-                let td = tr.insertCell();
-                td.appendChild(document.createTextNode(`${i},${j}` ));
-                td.style.border = '1px solid black';
-            }     
-    }
-    body.appendChild(tbl);
-}
-
-tableCreate(4,4);
